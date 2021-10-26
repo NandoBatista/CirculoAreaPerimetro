9cea6daad6972cdc69e48add4e8390f35d471769
Fernando Batista <90981307+FernandoBatista123@users.noreply.github.com>
2021-10-26 10:10:50 -0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,19 @@ import java.util.*
 
 fun main(args: Array<String>) {
     var diametro = 0.0
+    var perimetro = 0.0
     var area = 0.0
     var pi = 3.14
     var raio = 0.0
     var leitura = Scanner(System.`in`)
 
 
-    print("Digite o valor do diâmetro da área: ")
+    print("Digite o valor do diâmetro do círculo: ")
     diametro = leitura.nextInt()
     raio = diametro/2
     area = pi * raio * raio
+    perimetro = (raio * 2) * pi
 
-    println("A área de um círculo de raio $raio é $area")
+    println("Com base no diametro $diametro da área de um círculo, temos qu a area é igual a $area e seu perimetro é igual a $perimetro")
 
 }
